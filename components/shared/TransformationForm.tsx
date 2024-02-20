3d39cf41405c4c67b171b901139370ab19ade55d
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-02-20 20:35:11 +0530

    Setup: Cloudinary with the its upload image functionality

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -23,7 +23,7 @@ import {
 } from "@/constants";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { CustomField } from "./CustomField";
-import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -44,7 +44,7 @@ const TransformationForm = ({
   const transformationType = transformationTypes[type];
 
   // Use states
-  const [Image, setImage] = useState(data);
+  const [image, setImage] = useState(data);
   const [newTransformation, setNewTransformation] =
     useState<Transformations | null>(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -220,6 +220,23 @@ const TransformationForm = ({
             )}
           />
         )}
+
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
 
         <div className="flex flex-col gap-4">
           {/* Transforming */}
