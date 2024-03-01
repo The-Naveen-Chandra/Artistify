cff44a406f97a5363eee9d428514842212140254
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-03-01 22:46:20 +0530

    Dev: Search feature and collection gird for all the transformed images

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
-
-import { navLinks } from "@/constants";
 import Image from "next/image";
 
-const Home = () => {
+import { navLinks } from "@/constants";
+import { Collection } from "@/components/shared/Collection";
+import { getAllImages } from "@/lib/actions/image.actions";
+
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({
+    page,
+    searchQuery,
+  });
+
   return (
     <>
       <section className="home">
@@ -25,6 +35,15 @@ const Home = () => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   );
